410b7e478d24089a8ca3fd60abb47987eddd30d4
Alexander0209 <a.sklyarov99@gmail.com>
2021-05-15 22:54:04 +0300

    js beginnig

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})  
+
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
